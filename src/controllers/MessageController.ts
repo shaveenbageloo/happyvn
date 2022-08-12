45e7ba31f643ef26c98b797dc25517af11ed4ed5
Shaveen Bageloo <shaveen.bageloo@standardbank.co.za>
2022-08-12 13:56:41 +0200

    Completed the swagger api docs

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -1,29 +1,45 @@
-import { JsonController, Param, Body, Get, Post } from "routing-controllers";
+import { JsonController, Body, Get, Post } from "routing-controllers";
 import "reflect-metadata";
-import { validate, ValidationError } from "class-validator";
 import { Message } from "../models/message";
-import e from "express";
 
 const batchTimeout = process.env.BATCH_TIMEOUT || 10000;
-
-// const { OpenApiValidator } =
-//   require("express-openapi-validate").OpenApiValidator;
-// const jsYaml = require("js-yaml");
-// const fs = require("fs");
-// const openApiDocument = jsYaml.safeLoad(
-//   fs.readFileSync("openapi.yaml", "utf-8")
-// );
-
 const messagesArray: Message[] = [];
 
 @JsonController()
 export class MessageController {
   constructor() {
-    console.log("Constructor Method run");
+    console.log("Constructor Initialized.");
     setInterval(() => {
       console.log("Timer lapsed.");
       console.log(this.aggregatedMessages());
     }, Number(batchTimeout));
+  }
+
+  @Post("/message")
+  async post(@Body() message: Message) {
+    const JSONMessage = JSON.parse(JSON.stringify(message));
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!(new Date(JSONMessage.timestamp).getTime() > 0)) {
+          throw new Error("Invalid Timestamp");
+        }
+
+        const msgLength = messagesArray.push(
+          new Message(
+            JSONMessage.destination,
+            JSONMessage.text,
+            JSONMessage.timestamp
+          )
+        );
+        console.log("Message Length now: ", msgLength);
+
+        resolve(null);
+      } catch (error) {
+        console.log("Error when receiving the message: " + error);
+        reject("Error when receiving the message: " + error);
+      }
+    });
   }
 
   @Post("/aggregated-messages")
@@ -43,13 +59,22 @@ export class MessageController {
     };
 
     if (messagesArray.length != 0) {
+      // MAKE A COPY OF THE ORIGINAL MESSAGES ARRAY
+      // SO THAT IF THERE ARE NUMEROUS MESSAGES COMING IN FAST ENOUGH
+      // WE CAN CONTINUE RECIEVING THEM
       const copyMessagesArray = JSON.parse(
         JSON.stringify(messagesArray)
       ) as typeof messagesArray;
 
+      // CLEAR OUT ORIGINAL MESSAGESARRAY AS WE HAVE MADE A COPY OF IT
+      // AND LET NEW MESSAGES FILL UP FROM THE POST(/MESSAGES) API
       messagesArray.splice(0, messagesArray.length);
 
+      // DUE TO BE DECLARING THE JSON OBJECT ABOVE, THERE IS A DEFAULT
+      // BLANK ITEM ADDED TO THE BEGINING OF THE ARRAY.  I SIMPLY REMOVE THIS
       sendBatch.batches.pop();
+
+      // NOW WORK WITH THE COPIED ARRAY
       copyMessagesArray.forEach((message) => {
         let destinationFound = false;
 
@@ -76,27 +101,15 @@ export class MessageController {
         }
       });
 
+      // NOW CLEAR OUT THE COPIED MESSAGES AS WE HAVE CREATED OUR SENDBATCH
+      // JSON TO RETURN
       copyMessagesArray.splice(0, copyMessagesArray.length);
-      return JSON.stringify(sendBatch);
+      return JSON.parse(  JSON.stringify(sendBatch));
     } else {
+      // THIS METHOD IS CALLED EVERY x SECS, IT WILL RETURN A NULL
+      // IF THERE AREN'T ANY MESSAGES TO SEND
       return null;
     }
-  }
-
-  @Post("/message")
-  async post(@Body() message: Message) {
-    const JSONMessage = JSON.parse(JSON.stringify(message));
-
-    const msgLength = await messagesArray.push(
-      new Message(
-        JSONMessage.destination,
-        JSONMessage.text,
-        JSONMessage.timestamp
-      )
-    );
-    console.log("Message Length now: ", msgLength);
-
-    return null;
   }
 
   @Get("/health")
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { createExpressServer } from "routing-controllers";
 import { MessageController } from "./controllers/MessageController";
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDocs = YAML.load("src/openapi.yaml");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -11,7 +14,7 @@ const app = createExpressServer({
   controllers: [MessageController],
 });
 
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 app.listen(`${PORT}`, () => {
   console.log("Server started!");
